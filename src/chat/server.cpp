f3632419969f12990a181a53b6660795d1a4d9e9
Kritin Gupta <kritin.gupta.23@gmail.com>
2026-09-01 14:18:28 +0530

    PHASE 1: User Interface bugfixes
    This patches the UI implementation and fixes many bugs in it
    
    ChatGPT Chat:
    https://chatgpt.com/share/6a969182-4338-83e8-a4a5-647952daf1fa

--- src/chat/server.cpp
+++ src/chat/server.cpp
@@ -1,6 +1,8 @@
 #include "server.h"
+
 #include <iostream>
 #include <memory>
+
 #include "utils/Packet.hh"
 
 void Server::handleConnect() {
@@ -10,7 +12,7 @@ void Server::handleConnect() {
 void Server::handleLogin(SID sid, std::unique_ptr<FieldReqPacket>&& pkt) {
     std::string requestedUname = pkt->getField();
     ControlPacket reply(ControlField::ERROR, pkt->seq);
-    if(usernames.contains(requestedUname)) {
+    if (usernames.contains(requestedUname)) {
         reply.setControlField(ControlField::NACK);
     } else {
         reply.setControlField(ControlField::ACK);
@@ -42,18 +44,18 @@ void Server::handleMessage(SID sid, std::unique_ptr<MessagePacket>&& pkt) {
 
 int Server::run() {
     while (true) {
-        std::cerr   << "Waiting for new connections..." << std::endl;
+        // std::cerr << "Waiting for new connections..." << std::endl;
         handleConnect();
 
         NetworkHandler* handler = connections.waitForRead();
-        
+
         if (!handler) {
             continue;
         }
-        std::cerr << "Received packet from connection " << handler->getSocket() << std::endl;
-        std::cerr << "Handler pointer: " << handler << std::endl;
+        std::cerr << "Received packet from connection " << handler->getSocket()
+                  << std::endl;
 
-        SID sid = 0;
+        SID sid = -1;
 
         // Find the SID corresponding to the ready handler.
         for (size_t i = 0; i < connections.size(); ++i) {
@@ -66,65 +68,46 @@ int Server::run() {
         auto pkt = handler->receivePacket();
 
         if (!pkt) {
-            std::cerr
-                << "Failed to receive packet from connection "
-                << sid << '\n';
+            std::cerr << "Failed to receive packet from connection " << sid
+                      << '\n';
             continue;
         }
         std::cerr << "Received packet of type "
-                  << static_cast<int>(pkt->mPacketType)
-                  << " from connection "
+                  << static_cast<int>(pkt->mPacketType) << " from connection "
                   << sid << '\n';
 
         switch (pkt->mPacketType) {
-
             case PacketType::FIELD_REQ: {
                 auto fieldReqPkt =
-                    getDerivedPacket<FieldReqPacket>(
-                        std::move(pkt));
+                    getDerivedPacket<FieldReqPacket>(std::move(pkt));
 
-                handleLogin(
-                    sid,
-                    std::move(fieldReqPkt));
+                handleLogin(sid, std::move(fieldReqPkt));
 
                 break;
             }
 
             case PacketType::REQUEST: {
-                auto reqPkt =
-                    getDerivedPacket<RequestPacket>(
-                        std::move(pkt));
+                auto reqPkt = getDerivedPacket<RequestPacket>(std::move(pkt));
 
                 if (reqPkt->mRequestType == RequestType::DISCONNECT) {
-                    handleQuit(
-                        sid,
-                        std::move(reqPkt));
+                    handleQuit(sid, std::move(reqPkt));
                 } else {
-
-                    getUserList(
-                        sid,
-                        std::move(reqPkt));
+                    getUserList(sid, std::move(reqPkt));
                 }
 
                 break;
             }
 
             case PacketType::MESSAGE: {
-                auto msgPkt =
-                    getDerivedPacket<MessagePacket>(
-                        std::move(pkt));
+                auto msgPkt = getDerivedPacket<MessagePacket>(std::move(pkt));
 
-                handleMessage(
-                    sid,
-                    std::move(msgPkt));
+                handleMessage(sid, std::move(msgPkt));
 
                 break;
             }
 
             default:
-                std::cerr
-                    << "Unexpected packet type from "
-                    << sid << '\n';
+                std::cerr << "Unexpected packet type from " << sid << '\n';
                 break;
         }
     }
